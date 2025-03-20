9b50eb5e785f05696a83866041932a4d463bac7e
Joao castro <joaocastro0429@gmail.com>
2025-03-20 18:51:34 -0300

    atualizando o projeto

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def home(request):
+    posts = Post.objects.all().order_by('-date_created')
+    return render(request,'home.html',{'posts':posts})
